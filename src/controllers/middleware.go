276935a602b9ebd1c52d43b85ead4159c22aac38
mothylag <decagufy30@gmail.com>
2025-11-30 15:22:31 -0600

    feat(middlewares): Adding JWTAuth Middleware

--- src/controllers/middleware.go
+++ src/controllers/middleware.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"dnd-storage/src/application/ports"
+	"dnd-storage/src/domain/services"
 	"net/http"
 	"slices"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -39,6 +41,43 @@ func AuthMiddleware(appRepo ports.AppRepository,keyService ports.KeyService,allo
 		}
 
 		c.Set("appClient",appClient)
+		c.Next()
+	}
+}
+
+func JwtAuthMiddleware(jwtService *services.JWTService,jwtSecret string,allowedRoles []string) gin.HandlerFunc{
+	return func(c *gin.Context){
+		tokenStr := c.GetHeader("Authorization")
+		if tokenStr == ""{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"Authorization required"})
+			c.Abort()
+			return
+		}
+		tokenStr = strings.TrimPrefix(tokenStr,"Bearer ")
+
+		claims,err := jwtService.ValidateToken(tokenStr,jwtSecret)
+		if err != nil{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"Invalid Token"})
+			c.Abort()
+			return
+		}
+		userRole,ok := claims["role"].(string)
+		if(!ok){
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"User not authorized"})
+			c.Abort()
+			return
+		}
+		authorized := slices.Contains(allowedRoles,userRole)
+		if !authorized{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"User not authorized"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userId",claims["id"])
+		c.Set("userName",claims["username"])
+		c.Set("userRole",claims["role"])
+
 		c.Next()
 	}
 }
--- src/domain/services/jwt.service.go
+++ src/domain/services/jwt.service.go
@@ -21,7 +21,7 @@ func (s *JWTService) GenerateToken(user entities.User,jwtSecret string) (string,
 		"id": user.ID,
 		"username":user.UserName,
 		"role":user.Role,
-		"exp":time.Now().Add(time.Hour * 1).Unix(),
+		"exp":time.Now().Add(time.Minute * 15).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
@@ -30,4 +30,22 @@ func (s *JWTService) GenerateToken(user entities.User,jwtSecret string) (string,
 		return "",fmt.Errorf("Error Signing token: %s",err)
 	}
 	return tokenStr,nil
+}
+
+func (s *JWTService) ValidateToken(tokenStr,jwtSecret string) (jwt.MapClaims,error){
+	token,err := jwt.Parse(tokenStr,func(t *jwt.Token)(any,error){
+		if _,ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil,fmt.Errorf("Unexpected signing method")
+		}
+		return []byte(jwtSecret),nil
+	})
+	if err != nil{
+		return nil,err
+	}
+
+	if claims,ok :=token.Claims.(jwt.MapClaims); ok && token.Valid{
+		return claims,nil
+	}
+
+	return nil, fmt.Errorf("Invalid token")
 }
